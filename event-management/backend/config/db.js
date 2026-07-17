fc6308538c693319d7ec9f0b0809fb51ca155e83
mihir <vaidyamihir999@gmail.com>
2026-07-17 16:26:57 +0530

    Done

--- event-management/backend/config/db.js
+++ event-management/backend/config/db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/event_management");
-    console.log("MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Atlas Connected");
   } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
+    console.error("MongoDB Atlas connection failed:", error.message);
     process.exit(1);
   }
 };
